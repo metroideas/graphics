fc62864098470556c5c3dac70b76ef66dfe5a593
David Morton <michaeldavidmorton@gmail.com>
2016-04-13 14:15:07 -0400

    Updated quad/scatterplot of HCDE spending and accountability data
    Squashed commit of the following:
    
    commit 2bf10443d0f508b0b68d28e7fb23025539490e43
    Author: David Morton <michaeldavidmorton@gmail.com>
    Date:   Wed Apr 13 14:13:12 2016 -0400
    
        Mobile adjustments, clipped tooltip fix
    
    commit c7af5d6bb3781408e8b6bfbd041eb1523b637196
    Author: David Morton <michaeldavidmorton@gmail.com>
    Date:   Wed Apr 13 13:31:47 2016 -0400
    
        data update
    
    commit 61f529713ab24250d3cd4004ab5dbce6cef1bb92
    Author: David Morton <michaeldavidmorton@gmail.com>
    Date:   Wed Apr 13 11:52:58 2016 -0400
    
        Revisions, axis labels
    
    commit 047db35a504c9efc0b41748596863ffadb96f573
    Author: David Morton <michaeldavidmorton@gmail.com>
    Date:   Tue Apr 12 17:32:33 2016 -0400
    
        Updates to scatterplot-hcde-spending-accountability:
          - Adds final data
          - Quad labels (wrap on mobile)
          - Chart dimensions
          - Average line uses mean
          - Lead copy
    
    commit cb9f5d9ac57c8a61458be6cab7cbf901df2b126b
    Author: David Morton <michaeldavidmorton@gmail.com>
    Date:   Tue Apr 12 11:11:42 2016 -0400
    
        Update data structure

--- charts/scatterplot-hcde-spending-accountability/graphic.js
+++ charts/scatterplot-hcde-spending-accountability/graphic.js
@@ -21,15 +21,15 @@
     var mobile = (+container.offsetWidth <= 414);
 
     var margin = {
-      top: 16,
-      right: 32,
+      top: 0,
+      right: 16,
       bottom: 32,
-      left: 32,
+      left: 16,
       width: function() { return this.right + this.left; },
       height: function() { return this.top + this.bottom; }
     };
     
-    var ratio  = (mobile) ? { width: 1, height: 1 } : { width: 1, height: 1 };
+    var ratio  = (mobile) ? { width: 1, height: 1 } : { width: 1, height: 0.8 };
     
     var width  = function() {
       if (!containerWidth) {
@@ -52,31 +52,28 @@
 
     // Horizontal scale for average funding
     x = d3.scale.linear()
-      .domain(d3.extent(data, function(d) { return d.average; }))
+      .domain([3000,12000])
       .range([0, width])
       .nice();
 
     // Vertical scale for test scores
     y = d3.scale.linear()
-      .domain([0, 1])
-      .range([height, 0])
-      .nice();
+      .domain([-.5, .5])
+      .range([height, 0]);
 
     xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom")
-      .tickValues([7500])
+      .tickValues([7400])
       .tickSize(-height, 0, 0)
-      .tickFormat(d3.format("$,"))
-      ;
+      .tickFormat(d3.format("$,"));
 
     yAxis = d3.svg.axis()
       .scale(y)
       .orient("right")
-      .tickValues([.5])
+      .tickValues([0])
       .tickSize(-width, 0, 0)
       .tickFormat(d3.format("%.1"));
-    
 
     svg = d3.select(chart).append("svg")
         .attr("width", width + margin.width())
@@ -84,18 +81,20 @@
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // Axes and labels
+    // ---------------------------------------------------------------------------
+    svg.append("rect")
+      .attr("class", "box")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", width)
+      .attr("height", height);
+
     // Adds x axis
     svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
-    
-    // x axis label
-    svg.append("text")
-        .attr("x", width / 2)
-        .attr("y", -6)
-        .attr("text-anchor", "middle")
-        .text("Funding per student");
 
     // Adds y axis
     svg.append("g")
@@ -103,75 +102,86 @@
       .call(yAxis)
       .attr("transform", "translate(" + width + ",0)");
 
-    // y axis label
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -height / 2)
-      .attr("y", -6)
-      .attr("text-anchor", "middle")
-      .text("Proficiency");
+    // Remove y axis label
+    d3.select("g.y.axis .tick text").remove();
 
-    // Draws initial scatterplot
+    // Quad labels
+    if (!mobile) {
+      svg.append("text")
+        .attr("class", "quadrant-label")
+        .attr("x", x(x.domain()[0]) + 12)
+        .attr("y", y(y.domain()[1]))
+        .attr("dy", 24)
+        .text("Low spending, high achievement");
+
+      svg.append("text")
+        .attr("class", "quadrant-label")
+        .attr("x", x(x.domain()[1]) - 12)
+        .attr("y", y(y.domain()[1]))
+        .attr("text-anchor", "end")
+        .attr("dy", 24)
+        .text("High spending, high achievement");
+
+      svg.append("text")
+        .attr("class", "quadrant-label")
+        .attr("x", x(x.domain()[0]) + 12)
+        .attr("y", y(y.domain()[0]))
+        .attr("dy", -24)
+        .text("Low spending, low achievement");
+
+      svg.append("text")
+        .attr("class", "quadrant-label")
+        .attr("x", x(x.domain()[1]) - 12)
+        .attr("y", y(y.domain()[0]))
+        .attr("dy", -24)
+        .attr("text-anchor", "end")
+        .text("High spending, low achievement");
+    }
+     
+    // x and y axis label
+    svg.append("text")
+      .attr("class", "axis")
+      .attr("x", (mobile) ? width / 2 : 12)
+      .attr("y", height)
+      .attr("dy", (mobile) ? 24 : 12)
+      .attr("text-anchor", (mobile) ? "middle" : "left")
+      .text("Spending per student");
+
+    svg.append("text")
+      .attr("class", "axis")
+      .attr("x", -height / 2)
+      .attr("y", 0)
+      .attr("dy", -6)
+      .attr("text-anchor", "middle")
+      .attr("transform", "rotate(-90)")
+      .text("Student proficiency");
+    
+    // Initial scatterplot
+    // ---------------------------------------------------------------------------
     schools = svg.selectAll("circle.school")
         .data(data)
       .enter().append("circle")
         .attr("class", "school")
         .call(attributes);
 
-    // Establishes scatterplot attributes and hover functions
+    // Scatterplot attributes and hover functions
     function attributes() {
       return this.attr({
         cx: function(d) { return x(d.average); },
         cy: function(d) { return y(testScore(d)); },
-        r:  6
+        r:  4
       }).call(hover);
     }
 
-    // Sets schools' y positions
+    // Set schools' y position
     function testScore(d) {
       var subject = (d3.select("#english").classed("primary")) ? "english" : "math";
 
       return d[subject];
     }
 
-    // Input transitions
+    // Inputs
     // ---------------------------------------------------------------------------
-    // Transition settings
-    function transition(transition) {
-      return transition.delay(100).duration(300);
-    }
-
-    // Data filter and joins
-    function filterSchools() {
-      var
-      grades   = data,
-      title    = d3.select("#title").classed("primary"),
-      selected = d3.select("#grade-level-select").property('value');
-
-      if (selected != "all") {
-        grades = grades.filter(function(d) { return d.grade == selected; });
-      }
-      
-      if (title) {
-        grades = grades.filter(function(d) { return d.title; });
-      }
-
-      schools = svg.selectAll("circle.school")
-        .data(grades, function(d) { return d.key; });
-
-      schools.exit()
-        .transition().call(transition)
-        .attr("r", 0)
-        .remove();
-      
-      schools.enter().append("circle")
-        .attr("class", "school")
-        .attr("r", 0)
-        .transition().call(transition);
-        
-      schools.call(attributes);
-    }
-
     // Grade selector
     d3.select("#grade-level-select").on("change", filterSchools);
 
@@ -204,27 +214,68 @@
         .attr("cy", function(d) { return y(testScore(d)); });
     });
 
+    // Transition settings
+    function transition(transition) {
+      return transition.delay(100).duration(300);
+    }
+
+    // Data filter and joins
+    function filterSchools() {
+      var
+      grades   = data,
+      title    = d3.select("#title").classed("primary"),
+      selected = d3.select("#grade-level-select").property('value');
+
+      if (selected != "all") {
+        grades = grades.filter(function(d) { return d.grade == selected; });
+      }
+      
+      schools = svg.selectAll("circle.school")
+        .data(grades, function(d) { return d.key; });
+
+      schools.exit()
+        .transition().call(transition)
+        .attr("r", 0)
+        .remove();
+      
+      schools.enter().append("circle")
+        .attr("class", "school")
+        .attr("r", 0)
+        .transition().call(transition);
+        
+      schools.call(attributes)
+        .style("fill-opacity", function(d) {
+          return (title && d.title || !title) ? 0.7 : 0.1;
+        });
+    }
+
     // Tooltip called by attributes() for mouseover and mouseout
     // ---------------------------------------------------------------------------
+    
     function hover() {
       this.on("mouseover", function(d) {
-        var
-        tooltip = d3.select("#tooltip").classed("hidden", false),
-        left    = d3.event.pageX,
-        top     = d3.event.pageY
-        ;
+        var tooltip = d3.select("#tooltip").classed("hidden", false);
 
         // Text values
         tooltip.select("[data-item=name]").html(d.name);
+        console.log(d.name);
         tooltip.select("[data-item=average]").html(d3.format("$,.0f")(d.average));
-        tooltip.select("[data-item=english]").html(d3.format("%")(d.english));
-        tooltip.select("[data-item=math]").html(d3.format("%")(d.math));
+        tooltip.select("[data-item=english]").html(d3.format("%")(d.scores.english));
+        tooltip.select("[data-item=math]").html(d3.format("%")(d.scores.math));
 
         // Position
-        tooltip.style({
-          top: top  + 15 + "px",
-          left: left - 100 + "px"
-        });
+        tooltip
+          .style("left", function() {
+
+            var left = d3.event.pageX;
+            left -= (left >= window.innerWidth * .75) ? 200 : 100;
+
+            return left + "px";
+          })
+          .style("top", function() {
+            var y = d3.event.pageY;
+            return y + 15 + "px";
+          });
       })
 
       .on("mouseout", function() {
@@ -243,14 +294,14 @@
     
     data = csv.map(function(row) {
       return {
-        key: key++,
-        name: row.school,
-        title: (row.title == 1),
-        grade: row.grade,
-        adm: row.adm,
-        math: row.math,
-        english: row.english,
-        average: row.average
+        key:     key++,
+        name:    row.name,
+        title:   (row.title == 1),
+        grade:   row.grade,
+        math:    row.math_difference,
+        english: row.english_difference,
+        average: row.average,
+        scores:  { math: row.math, english: row.english }
       };
     });
 
